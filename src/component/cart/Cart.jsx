2dbaf45e66295df21ab8751f76f9e6c6f4d99cdb
afrojajisha <afrojajisha2@gmail.com>
2023-08-17 23:15:30 +0600

    show product update from cart

--- src/component/cart/Cart.jsx
+++ src/component/cart/Cart.jsx
@@ -10,9 +10,24 @@ const Cart = ({cart}) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart){
-        totalPrice = totalPrice + product.price;
-        totalShipping = totalShipping + product.shipping;
+    // method -2 (if) easiar version to check product quantity, if product quntity  is zero it will set the quantity to one ***(this method is used jehetu primarily product quantity 0 te set kora thake tai  product.quanty diye gun korle shobgular man/value 0 hoye jabe)
+
+
+        // if(product.quantity === 0){
+        //     product.quantity = 1;
+        // }
+
+// method -1 (||1) easiar version to check product quantity, if product quntity  is zero it will set the quantity to one
+
+        // product.quantity = product.quantity || 1;
+
+
+
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping * product.quantity;
+        quantity = quantity + product.quantity;
     }
 
     const tax = totalPrice*7 /100;
@@ -21,7 +36,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
             <h4>Order Summary </h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping Charge: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
